ef5e1aa5a64f2956ba40e2162fc2a8e88ac4b125
EthanKHenning <150199069+EthanKHenning@users.noreply.github.com>
2023-11-20 22:57:10 -0800

    Update main.cpp

--- Code/main.cpp
+++ Code/main.cpp
@@ -3,12 +3,14 @@
 using namespace std;
 
 //divide by 2 to make run faster during testing 
+
+/*
 VideoMode::getDesktopMode().width;
 VideoMode::getDesktopMode().height;
+*/
 
 /*
 main()
 {
 	
 }
-*/
