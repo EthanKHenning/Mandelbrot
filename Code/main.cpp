96d770e0c8e9f04f8187c9a74367e793f9050be1
Your Name <you@example.com>
2023-11-20 19:52:23 -0800

    better main

--- Code/main.cpp
+++ Code/main.cpp
@@ -1,10 +1,11 @@
 #include "ComplexPlane.h" 
-
-//technically edited
+#include <iostream>
+using namespace std;
 
 //divide by 2 to make run faster during testing 
 VideoMode::getDesktopMode().width;
 VideoMode::getDesktopMode().height;
+
 
 main()
 {
