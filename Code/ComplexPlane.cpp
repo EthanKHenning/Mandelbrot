e3374ea88fed38bae0c586d5d51637cb8a358260
Your Name <you@example.com>
2023-11-20 23:51:39 -0800

    rough updateRender()

--- Code/ComplexPlane.cpp
+++ Code/ComplexPlane.cpp
@@ -18,22 +18,34 @@ void ComplexPlane::draw(RenderTarget& target, RenderStates states) const
     target.draw(m_vArray);
 }
 
+
 void ComplexPlane::updateRender()
 {
     if (m_state == State::CALCULATING)
     {
-        // j is our x
-        // i is our y
-        for()
+        // Double for loop allows us to iterate through all pixels
+        for (int i = 0; i < m_pixel_size.y; ++i)
         {
-            for()
+            for (int j = 0; j < m_pixel_size.x; ++j)
             {
+                m_vArray[j + i * m_pixel_size.x].position = { j, i };
 
+                // Use ComplexPlane::mapPixelToCoords to find the Vector2f coordinate
+                Vector2f coordinate = mapPixelToCoords(Vector2i(j, i));
+
+                // Call ComplexPlane::countIterations
+                int iterations = countIterations(coordinate);
+                Uint8 r, g, b;
+                iterationsToRGB(iterations, r, g, b);
+
+                // Sets the color for the point/vertex
+                m_vArray[j + i * m_pixel_size.x].color = { 255, r, g, b };  
             }
         }
-        
-    }
 
+        // Set the state to DISPLAYING
+        m_state = State::DISPLAYING;
+    }
 }
 
 //keeps track of each time the user scrolls the mouse wheel up, for each upward scroll it will generate a new version of the plane enlarged proprotionally
