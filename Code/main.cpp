fa2e224b818b3d9806ff9d39f304b3c1142c1cdf
Your Name <you@example.com>
2023-11-20 19:08:14 -0800

    Organized Files

---
+++ Code/main.cpp
@@ -0,0 +1,12 @@
+#include "ComplexPlane.h" 
+
+//technically edited
+
+//divide by 2 to make run faster during testing 
+VideoMode::getDesktopMode().width;
+VideoMode::getDesktopMode().height;
+
+main()
+{
+	
+}
